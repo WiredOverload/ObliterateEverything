fa0abdc8b397237e05b680145ae7297e9bf0f33b
Michael Hodges <Michael Hodges@mchuggin>
2016-10-06 11:39:13 -0600

    added all files available on 8/22/2016

--- src/obliterateeverything/Player.java
+++ src/obliterateeverything/Player.java
@@ -5,10 +5,16 @@
  */
 package obliterateeverything;
 
+import java.io.File;
 import java.util.ArrayList;
+import javafx.scene.Group;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.media.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.transform.Rotate;
+
 
 /**
  *
@@ -16,6 +22,8 @@ import javafx.scene.transform.Rotate;
  */
 public class Player {
 
+    private static final boolean DEBUG = false;
+    
     private int energy; //currency
     private int team;
     private ArrayList<Spawner> spawners;
@@ -26,6 +34,8 @@ public class Player {
     private Image target = new Image("target.png"); //debug only
     private Image boundingBox = new Image("boundingBox.png"); //debug only
     Rotate r; // needed for sprite rotation
+    
+    
 
     public Player() {
         this.energy = 500;
@@ -84,10 +94,20 @@ public class Player {
     }
 
     //methods
-    void update(Player enemy) {
+    void update(Player enemy, Group root) {
         energy++;
         for (int i = 0; i < spawners.size(); i++) {
-            //System.out.println(this.team);
+            if (spawners.get(i).getHealth() <= 0) {
+                root.getChildren().remove(spawners.get(i).getgHealth());
+                root.getChildren().remove(spawners.get(i).getrHealth());
+            }
+            for (int j = 0; j < spawners.get(i).getShips().size(); j++)
+            {
+                if (spawners.get(i).getShips().get(j).getHealth() <= 0) {
+                    root.getChildren().remove(spawners.get(i).getShips().get(j).getgHealth());
+                    root.getChildren().remove(spawners.get(i).getShips().get(j).getrHealth());
+                }
+            }
             spawners.get(i).update(enemy);
             if (spawners.get(i).getHealth() <= 0) {
                 spawners.remove(i);
@@ -95,30 +115,64 @@ public class Player {
         }
     }
 
-    void render(GraphicsContext gc) {
+    void render(GraphicsContext gc, Group root) {
         for (int i = 0; i < spawners.size(); i++) {
             gc.drawImage(spawner, spawners.get(i).getX(), spawners.get(i).getY());
+            if (spawners.get(i).getCooldown() < 0) {
+                root.getChildren().add(spawners.get(i).getgHealth());
+                root.getChildren().add(spawners.get(i).getrHealth());
+            }
             for (int j = 0; j < spawners.get(i).getShips().size(); j++) {
                 gc.save();
-                r = new Rotate(spawners.get(i).getShips().get(j).getAngle(), spawners.get(i).getShips().get(j).getX() + 8, spawners.get(i).getShips().get(j).getY() + 8);
+                r = new Rotate((spawners.get(i).getShips().get(j).getAngle() + 90) % 360, spawners.get(i).getShips().get(j).getX() + 8, spawners.get(i).getShips().get(j).getY() + 8);
                 gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
                 gc.drawImage(ship, spawners.get(i).getShips().get(j).getX(), spawners.get(i).getShips().get(j).getY());
                 gc.restore();
+                
                 if (spawners.get(i).getShips().get(j).getHitTime() > 0) {
                     gc.drawImage(hit, spawners.get(i).getShips().get(j).getHitX(), spawners.get(i).getShips().get(j).getHitY());
                 }
-                if(this.team == 1)
-                gc.drawImage(target, spawners.get(i).getShips().get(j).getTargetX(), spawners.get(i).getShips().get(j).getTargetY());
-                //gc.drawImage(boundingBox, spawners.get(i).getShips().get(j).getX(), spawners.get(i).getShips().get(j).getY());
-
+                
+                //health bars
+                if (spawners.get(i).getShips().get(j).getCooldown() < 0) {
+                    root.getChildren().add(spawners.get(i).getShips().get(j).getgHealth());
+                    root.getChildren().add(spawners.get(i).getShips().get(j).getrHealth());
+                }
+                
+                //draws target lines
+                if(DEBUG)
+                {
+                    if (this.team == 0) {
+                        spawners.get(i).getShips().get(j).getLine().setStroke(Color.RED);
+                    }
+                    else {
+                        spawners.get(i).getShips().get(j).getLine().setStroke(Color.BLUE);
+                    }
+                    
+                    if (spawners.get(i).getShips().get(j).getCooldown() < 0) {
+                        root.getChildren().add(spawners.get(i).getShips().get(j).getLine());
+                    }
+                    if (spawners.get(i).getShips().get(j).getHealth() <= 0) {
+                        root.getChildren().remove(spawners.get(i).getShips().get(j).getLine());
+                    }
+                    gc.drawImage(boundingBox, spawners.get(i).getShips().get(j).getX(), spawners.get(i).getShips().get(j).getY());
+                }
+                
+                //if(this.team == 1)
+                //gc.drawImage(target, spawners.get(i).getShips().get(j).getTargetX(), spawners.get(i).getShips().get(j).getTargetY());
+                
+                //Line line = new Line(0,0,100,100);
+                //root.getChildren().add(line);
+                
                 for (int k = 0; k < spawners.get(i).getShips().get(j).getLasers().size(); k++) {
                     gc.save();
-                    r = new Rotate(spawners.get(i).getShips().get(j).getLasers().get(k).getAngle(), spawners.get(i).getShips().get(j).getLasers().get(k).getX() + 1, spawners.get(i).getShips().get(j).getLasers().get(k).getY() + 4);
+                    r = new Rotate((spawners.get(i).getShips().get(j).getLasers().get(k).getAngle() + 90) % 360, spawners.get(i).getShips().get(j).getLasers().get(k).getX() + 1, spawners.get(i).getShips().get(j).getLasers().get(k).getY() + 4);
                     gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
                     gc.drawImage(laser, spawners.get(i).getShips().get(j).getLasers().get(k).getX(), spawners.get(i).getShips().get(j).getLasers().get(k).getY());
                     gc.restore();
                 }
             }
         }
+        //gc.drawImage(target,,); //mouse event testing
     }
 }
